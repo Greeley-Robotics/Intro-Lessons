ed963de06750d2281f3204bc1c1dc30f683b1d92
RZJournal <rzjournal@rzjournal.com>
2024-02-02 13:50:37 -0500

    HelloWorld

---
+++ Week 1/HelloWorld.java
@@ -0,0 +1,45 @@
+// Week 1 - Programming Basics in Java
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        // Welcome to Week 2!
+        System.out.println("Hello, Week 1!");
+
+        // Java Syntax and Coding Conventions
+        // Here's a simple Java program to print "Hello, World!"
+        System.out.println("Hello, World!");
+
+        // Data Types in Java
+        // int: Integer (whole numbers)
+        int age = 25;
+
+        // double: Double-precision floating-point numbers
+        double pi = 3.14;
+
+        // String: Textual data
+        String message = "Hello, Java!";
+
+        // boolean: Boolean values (true or false)
+        boolean isProgrammingFun = true;
+
+        // Displaying the values
+        System.out.println("Age: " + age);
+        System.out.println("Pi: " + pi);
+        System.out.println("Message: " + message);
+        System.out.println("Is Programming Fun? " + isProgrammingFun);
+
+        // Variable Declaration and Initialization
+        // Declaration
+        int anotherNumber;
+        double anotherPi;
+
+        // Initialization
+        anotherNumber = 42;
+        anotherPi = 3.14159;
+
+        // Displaying the values
+        System.out.println("Another Number: " + anotherNumber);
+        System.out.println("Another Pi: " + anotherPi);
+    }
+}
